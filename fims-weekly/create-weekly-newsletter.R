80a72e76765c3c0485b30ab0fd295e09831848ef
Elizabeth Perl <elizabeth.gugliotti@noaa.gov>
2025-07-29 17:31:08 -0400

    try carlos' suggestion

--- fims-weekly/create-weekly-newsletter.R
+++ fims-weekly/create-weekly-newsletter.R
@@ -75,36 +75,63 @@ title: "%s"
 date: "%s"
 # Fill categories in below. Look to previous FIMS weekly newsletters for examples.
 # These examples include: testing, projections, selectivity, diagnostics, etc.
-categories: [cat, cat]
-big_things: |
-  1. 
-  2. 
-  3. 
-announcements: |
-  - 
-  - 
-events:
-  - date: ""
-    title: ""
-    time: ""
-    location: ""
-    online:
-      label: ""
-      url: ""
-  - date: ""
-    title: ""
-    time: ""
-    location: ""
-    online:
-      label: ""
-      url: ""
-photo_caption: |
-  
-photo: images/extended_tests.png
-photo_alt: ""
+categories: 
+  - cat1
+  - cat2
 ---
-  
-{=include} ../_assets/fims-weekly-template.qmd',
+
+![](images/fims_weekly.png){fig-alt="FIMS hex icon and noaa logo with text saying FIMS Weekly"}
+
+<div style="height:3em;"></div>
+
+::: {.columns .equal-h2-cols}
+::: {.column width="70%"}
+
+<div class="header-wrap"><h2>THREE BIG THINGS THIS WEEK</h2></div>
+1. 
+2. 
+3. 
+
+:::
+
+::: {.column width="30%"}
+<div class="header-wrap"><h2>HELPFUL RESOURCES</h2></div>
+- [FIMS Outreach Activities](https://docs.google.com/spreadsheets/d/1g60HmAw8w_CIWBbQwTbq-oWs1ChdTTqvCREVQ8e66sE/edit?gid=0#gid=0)
+- [NMFS Open Science Calendar](https://calendar.google.com/calendar/embed?src=c_916b6948ef2ee8b7d49c28661efc0798303c2742be399c9290fa02d9320c769e%40group.calendar.google.com&ctz=America%2FNew_York)
+- [Implementation Team Notes](https://docs.google.com/document/d/10nSfbPaBF2p7wL2cr5lW7PGZxlZI4tbPiwQRC8JKaXk/edit?usp=drive_link)
+- [FIMS FAQs](https://noaa-fims.github.io/faq)
+- [NOAA-FIMS Discussions](http://github.com/orgs/NOAA-FIMS/discussions)
+:::
+
+:::
+
+::: {.columns}
+::: {.column}
+## FIMS ANNOUNCEMENTS
+- 
+- 
+:::
+
+::: {.column}
+## UPCOMING EVENTS
+### Tuesday, 
+**FIMS Code Club**
+Time: 13:00–14:00 E; 10:00–11:00 P; 9:00–10:00 AK; 7:00–8:00 H 
+Location: Virtual
+Online: [Google Meet](http://meet.google.com/ica-ieta-ndu)
+
+### Thursday, 
+**FIMS Seaside Chat**
+Time: 13:00–14:00 E; 10:00–11:00 P; 9:00–10:00 AK; 7:00–8:00 H
+Location: Virtual
+Online: [Google Meet](http://meet.google.com/scg-pyqi-ghj)
+:::
+:::
+
+## PHOTO OF THE WEEK
+![](images/PHOTO_NAME.png){height="150%" fig-alt="ALTERNATIVE TEXT"}
+
+',
     week_range, format(week_start, "%B %d, %Y")
     )
   writeLines(yaml, file_path) 
