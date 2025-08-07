8287848e7bdd20bd086bcb5102d5bf6393fb0d12
e-perl-NOAA <elizabeth.gugliotti@noaa.gov>
2025-08-07 17:29:33 -0400

    fix changelog

--- .github/change_log.R
+++ .github/change_log.R
@@ -13,6 +13,12 @@ fetch_releases <- function(api_url) {
   fromJSON(content(resp, as = "text", encoding = "UTF-8"))
 }
 
+# Transform direct PR/issue URLs to markdown link [#number](url)
+reformat_github_urls <- function(text) {
+  gsub("(https://github\\.com/NOAA-FIMS/FIMS/(issues|pull)/(\\d+))",
+       "[#\\3](\\1)", text, perl = TRUE)
+}
+
 # Write the changelog in Quarto Markdown format
 write_changelog <- function(releases, output_file) {
   header <- c(
@@ -26,6 +32,8 @@ write_changelog <- function(releases, output_file) {
     rel <- as.list(releases[i, ])
     rel_title <- glue("## {rel$name} ([release notes]({rel$html_url}))")
     rel_body <- if (!is.null(rel$body) && nzchar(rel$body)) rel$body else "*No release notes available.*"
+    rel_body <- gsub("(?m)^[ \t]*\\* ", "- ", rel_body, perl = TRUE)
+    rel_body <- reformat_github_urls(rel_body)
     lines <- c(lines, rel_title, rel_body, "", "---", "")
   }
   writeLines(lines, output_file, useBytes = TRUE)
